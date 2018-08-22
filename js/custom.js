00fd5d37d2e3de26977b77f3045ee7ce53b31f83
Chris O'Brien <chris@nkpadmin.com>
2018-08-22 16:34:19 -0700

    gdpr

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,21 @@ jQuery(document).ready(function($) {
 		controlNav: false
 	});
 
+	jQuery('.cookie-accept').click(function() {
+		jQuery('#cookie-disclaimer').hide();
+	});
+
+	$('.cookie-accept').click(function(){
+		$('#cookie_disclaimer').slideUp();
+
+		var nDays = 999;
+		var cookieName = "disclaimer";
+		var cookieValue = "true";
+
+		var today = new Date();
+		var expire = new Date();
+		expire.setTime(today.getTime() + 3600000*24*nDays);
+		document.cookie = cookieName+"="+escape(cookieValue)+";expires="+expire.toGMTString()+";path=/";
+	 });
+
 });
